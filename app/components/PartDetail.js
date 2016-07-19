b53fa5c6c966caa67126e120b2bef1a11dafe8d9
george francis <georgefrancis105@gmail.com>
2016-07-19 17:14:51 +1200

    Fix and add part routes

--- app/components/PartDetail.js
+++ app/components/PartDetail.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 export default (props) => {
 	return (
 		<span>
-			<li><Link to={`/part/${props.id}`}>Part Number: {props.partNumber} Description:{props.description} </Link></li>
+			<li><Link to={`/parts/${props.id}`}>Part Number: {props.partNumber} Description:{props.description} </Link></li>
 		</span>
 	)
 }
--- app/components/routes.js
+++ app/components/routes.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 import PartIndexContainer from './containers/PartIndexContainer'
+import HomeContainer from './containers/HomeContainer'
 
 export default (
 	<Router history={browserHistory}>
-		<Route path="/" component={PartIndexContainer} />
+		<Route path="/" component={HomeContainer} />
+		<Route path="/parts" component={PartIndexContainer} />
+		<Route path="/parts/:id" component={PartIndexContainer} />
 		
 	</Router>
 )
