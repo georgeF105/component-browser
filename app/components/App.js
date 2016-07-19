d70645f9a9649d48287d5225459e52e34d0fc6e0
george francis <georgefrancis105@gmail.com>
2016-07-19 16:09:59 +1200

    Setup webpack config and scripts

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import NavLink from './NavLink'
 import NavBarContainer from './containers/NavBarContainer'
-import Footer from './Footer'
 import routes from './routes'
 
 export default React.createClass({
