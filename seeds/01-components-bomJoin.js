a2d9144fa9c8a9d975623dd6c5f84fbbd46d02a9
george francis <georgefrancis105@gmail.com>
2016-07-20 13:50:28 +1200

    Merge seed files into one to fix dependency issues

--- seeds/01-components-bomJoin.js
+++ seeds/01-components-bomJoin.js
@@ -1,7 +1,8 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('components').del()
+  return knex('bomJoin').del()
+    .then(function() { return knex('components').del()})
     .then(function () {
       return Promise.all([
         // Inserts seed entries}
@@ -14,6 +15,19 @@ exports.seed = function(knex, Promise) {
         knex('components').insert({id: 7, partNumber: '100-00007', 'description': 'Wheels', 'revision':0}),
         knex('components').insert({id: 8, partNumber: '100-00008', 'description': 'Seat Back', 'revision':0}),
         knex('components').insert({id: 9, partNumber: '100-00009', 'description': 'Seat Base', 'revision':0})
-      ]);
-    });
-};
+      ])
+    })
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('bomJoin').insert({id: 1, assmId: 1, partId: 2, qyt: 1}),
+        knex('bomJoin').insert({id: 2, assmId: 1, partId: 3, qyt: 1}),
+        knex('bomJoin').insert({id: 3, assmId: 1, partId: 4, qyt: 1}),
+        knex('bomJoin').insert({id: 4, assmId: 1, partId: 5, qyt: 2}),
+        knex('bomJoin').insert({id: 5, assmId: 2, partId: 8, qyt: 1}),
+        knex('bomJoin').insert({id: 6, assmId: 2, partId: 9, qyt: 1}),
+        knex('bomJoin').insert({id: 7, assmId: 4, partId: 6, qyt: 1}),
+        knex('bomJoin').insert({id: 8, assmId: 4, partId: 7, qyt: 6})
+      ])
+    })
+}
