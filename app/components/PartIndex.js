6509633a779e150c2c046ebabe9268ab7644b3dd
george francis <georgefrancis105@gmail.com>
2016-08-08 13:39:07 +1200

    Refactor part preview selector to use PartIndex state

--- app/components/PartIndex.js
+++ app/components/PartIndex.js
@@ -3,14 +3,29 @@ import React from 'react'
 import PartTable from './PartTable'
 import PartPreview from './PartPreview'
 
-export default (props) => {
-  const id = parseInt(props.location.query.preview)
-  const part = props.parts.find(part => part.id === id)
+class PartIndex extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      part: null
+    }
+  }
 
-  return (
-    <div className='content-grid mdl-grid'>
-      <PartTable {...props} />
-      {part && <PartPreview part={part} />}
-    </div>
-  )
+  selectPreviewPart = (id) => {
+    this.setState({ part: this.props.parts.find(part => part.id === id) })
+    console.log('part', this.state.part)
+  }
+
+  render () {
+    const part = this.state.part//.find(part => part.id === id)
+    const parts = this.props.parts
+    return (
+      <div className='content-flexbox'>
+        <PartTable parts={parts} selector={this.selectPreviewPart} />
+        {part && <PartPreview part={part} />}
+      </div>
+    )
+  }
 }
+
+export default PartIndex
--- app/components/PartTable.js
+++ app/components/PartTable.js
@@ -4,6 +4,7 @@ import { DataTable, TableHeader, IconButton } from 'react-mdl'
 
 export default (props) => {
   const parts = props.parts
+  const selector = props.selector
   return (
     <div className="index-table">
       <div className="index-table-row heading">
@@ -11,9 +12,9 @@ export default (props) => {
         <div className="col-description">Description</div>
         <div className="col-revision">Revision</div>
       </div>
-      {parts.map(part => {
+      {parts.map( (part, key) => {
         return (
-          <div className="index-table-row part">
+          <div key={key} className="index-table-row part" value="1" onClick={selector.bind(this, part.id)} >
             <div className="col-partnum">{part.partNumber}</div>
             <div className="col-description">{part.description}</div>
             <div className="col-revision">{part.revision}</div>
