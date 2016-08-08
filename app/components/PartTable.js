1ef2573bbc45582be7b5ba587f5b4ee374c46ad2
george francis <georgefrancis105@gmail.com>
2016-08-08 15:41:50 +1200

    Refactor styling. Add sort icons

--- app/components/PartTable.js
+++ app/components/PartTable.js
@@ -1,22 +1,29 @@
 import React from 'react'
+import { IconButton } from 'react-mdl'
 
 export default (props) => {
   const parts = props.parts
   const selector = props.selector
   return (
     <div className='index-table'>
-      <div className='index-table-row heading'>
-        <div className='col-partnum'>Part Number</div>
-        <div className='col-description'>Description</div>
-        <div className='col-revision'>Revision</div>
+      <div className='index-table-row'>
+        <div className='index-col partnum heading'>Part Number
+          <IconButton name='keyboard_arrow_down'/>
+        </div>
+        <div className='index-col description heading'>Description
+          <IconButton name='sort'/>
+        </div>
+        <div className='index-col revision heading'>Revision
+          <IconButton name='sort'/>
+        </div>
       </div>
       <div className='index-table-body'>
       {parts.map((part, key) => {
         return (
           <div key={key} className='index-table-row part' value='1' onClick={selector.bind(this, part.id)} >
-            <div className='col-partnum'>{part.partNumber}</div>
-            <div className='col-description'>{part.description}</div>
-            <div className='col-revision'>{part.revision}</div>
+            <div className='index-col partnum'>{part.partNumber}</div>
+            <div className='index-col description'>{part.description}</div>
+            <div className='index-col revision'>{part.revision}</div>
           </div>
         )
       })}
