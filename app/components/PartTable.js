08f2f679391c7c17b1e0821e0beb0bae20ce46a8
george francis <georgefrancis105@gmail.com>
2016-08-08 13:45:38 +1200

    Fix linting

--- app/components/PartTable.js
+++ app/components/PartTable.js
@@ -1,23 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router'
-import { DataTable, TableHeader, IconButton } from 'react-mdl'
 
 export default (props) => {
   const parts = props.parts
   const selector = props.selector
   return (
-    <div className="index-table">
-      <div className="index-table-row heading">
-        <div className="col-partnum">Part Number</div>
-        <div className="col-description">Description</div>
-        <div className="col-revision">Revision</div>
+    <div className='index-table'>
+      <div className='index-table-row heading'>
+        <div className='col-partnum'>Part Number</div>
+        <div className='col-description'>Description</div>
+        <div className='col-revision'>Revision</div>
       </div>
-      {parts.map( (part, key) => {
+      {parts.map((part, key) => {
         return (
-          <div key={key} className="index-table-row part" value="1" onClick={selector.bind(this, part.id)} >
-            <div className="col-partnum">{part.partNumber}</div>
-            <div className="col-description">{part.description}</div>
-            <div className="col-revision">{part.revision}</div>
+          <div key={key} className='index-table-row part' value='1' onClick={selector.bind(this, part.id)} >
+            <div className='col-partnum'>{part.partNumber}</div>
+            <div className='col-description'>{part.description}</div>
+            <div className='col-revision'>{part.revision}</div>
           </div>
         )
       })}
